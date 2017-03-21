e140da9d08e577a6c5a2a0deb1c4b60880507c45
Beefstyles <glenwillis@gmail.com>
2017-03-21 20:29:39 +0800

    Added the cleaning process to question 1 and 2 train.

--- TokenRF.r
+++ TokenRF.r
@@ -29,9 +29,20 @@ QuestionCleanup <- function(x){
 
 trainClean = train
 trainClean$question1 = tolower(trainClean$question1)
-q1Docs = Corpus(VectorSource(trainClean$question1))
 
+#Clean question 1 and remove extraneous parts
+q1Docs = Corpus(VectorSource(trainClean$question1))
 q1Docs <- tm_map(q1Docs, removeWords,stopwords('en'))
+q1Docs <- tm_map(q1Docs,removePunctuation)
+q1Docs <- tm_map(q1Docs,stripWhitespace)
+trainClean$question1 = data.frame(text=sapply(q1Docs,as.character),stringsAsFactors = FALSE) #Adds corpus back to existing dataframe
+
+#Clean question 2 and remove extraneous parts
+q2Docs = Corpus(VectorSource(trainClean$question2))
+q2Docs <- tm_map(q2Docs, removeWords,stopwords('en'))
+q2Docs <- tm_map(q2Docs,removePunctuation)
+q2Docs <- tm_map(q2Docs,stripWhitespace)
+trainClean$question2 = data.frame(text=sapply(q2Docs,as.character),stringsAsFactors = FALSE)
 
 #train$question1 <- QuestionCleanup(train$question1)
 #train$question2 <- QuestionCleanup(train$question2)
