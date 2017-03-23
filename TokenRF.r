ec907c98df9c586e0f799643fa69cf959cc32c91
Beefstyles <glenwillis@gmail.com>
2017-03-23 20:52:27 +0800

    Was able to add the unnest_tokens in.

--- TokenRF.r
+++ TokenRF.r
@@ -31,15 +31,14 @@ trainClean = train
 trainClean$question1 = tolower(trainClean$question1)
 
 #Clean question 1 and remove extraneous parts
-q1Docs = Corpus(VectorSource(trainClean$question1))
+q1Docs <- Corpus(VectorSource(trainClean$question1))
 q1Docs <- tm_map(q1Docs, removeWords,stopwords('en'))
 q1Docs <- tm_map(q1Docs,removePunctuation)
 q1Docs <- tm_map(q1Docs,stripWhitespace)
-#trainClean$question1 = data.frame(text=sapply(q1Docs,as.character),stringsAsFactors = FALSE) #Adds corpus back to existing dataframe
-trainClean$question1 = data.frame(text=sapply(q1Docs,function(i)i),stringsAsFactors = FALSE) #Adds corpus back to existing dataframe
+trainClean$question1 = data.frame(text=sapply(q1Docs,as.character),stringsAsFactors = FALSE) #Adds corpus back to existing dataframe
 
 #Clean question 2 and remove extraneous parts
-q2Docs = Corpus(VectorSource(trainClean$question2))
+q2Docs <- Corpus(VectorSource(trainClean$question2))
 q2Docs <- tm_map(q2Docs, removeWords,stopwords('en'))
 q2Docs <- tm_map(q2Docs,removePunctuation)
 q2Docs <- tm_map(q2Docs,stripWhitespace)
@@ -52,7 +51,24 @@ tokens_q1 <- trainClean %>%
   
 tokens_q1 <- tokens_q1[trainClean, on = "id"]
 colnames(tokens_q1)[1:3] <- c("id1","word1","n1")
-#train$question1 <- QuestionCleanup(train$question1)
-#train$question2 <- QuestionCleanup(train$question2)
+tokens_q1 <- tokens_q1[,c("id1", "question1", "word1", "n1"),with = FALSE]
+
+# calculate tf-idf weights
+tf.idf1 <- tokens_q1 %>% bind_tf_idf(word1, question1, n1) %>%
+                                select(id1, question1, word1, tf, idf, tf_idf)
+
+###  for question2
+tokens_q2 <- trainClean %>%
+  unnest_tokens(word, question2, drop = FALSE, token = "regex", pattern = " ") %>%
+  count(id, word) %>%
+  ungroup()
+tokens_q2 <- tokens_q2[df, on = "id"]
+colnames(tokens_q2)[1:3] <- c("id2", "word2", "n2")
+tokens_q2 <- tokens_q2[,c("id2", "question2", "word2", "n2"),with=FALSE]
+# calculate tf-idf weights
+tf.idf2 <- tokens_q2 %>% bind_tf_idf(word2, question2, n2) %>%
+                         select(id2, question2, word2, tf, idf, tf_idf)
+
+
 
 
